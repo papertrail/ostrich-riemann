fda184e5a5445b059b4bc3b960fbbe7c3ea6f684
Eric Lindvall <eric@5stops.com>
2014-06-16 18:19:08 -0700

    Simplifying some of the code

--- src/main/scala/com/papertrailapp/ostrich/RiemannStatsLogger.scala
+++ src/main/scala/com/papertrailapp/ostrich/RiemannStatsLogger.scala
@@ -2,72 +2,85 @@ package com.papertrailapp.ostrich
 
 import com.twitter.ostrich.admin.{AdminHttpService, PeriodicBackgroundProcess}
 import com.twitter.util.Duration
-import com.twitter.conversions.time._
 import com.aphyr.riemann.client.RiemannClient
 import com.twitter.ostrich.stats.{StatsListener, StatsCollection}
 import java.net.InetAddress
 import collection.JavaConversions._
 import com.twitter.conversions.time._
 import com.twitter.ostrich.admin.config.{StatsReporterConfig, JsonStatsLoggerConfig}
+import com.twitter.logging.Logger
 
 class RiemannStatsLoggerConfig(var period: Duration = 1.minute,
                                var prefix: Option[String] = None,
                                var host: String = "localhost",
                                var localHostname: Option[String] = None,
                                var shortLocalHostname: Boolean = true,
-                               var port: Int = 5555)
+                               var port: Int = 5555,
+                               var tags: Seq[String] = Seq())
   extends StatsReporterConfig {
 
   def apply() = { (collection: StatsCollection, admin: AdminHttpService) =>
-    new RiemannStatsLogger(host, port, prefix, localHostname, shortLocalHostname, period, collection)
+    new RiemannStatsLogger(host, port, prefix, localHostname, shortLocalHostname, tags, period, collection)
   }
 }
 
-class RiemannStatsLogger(val host: String, val port: Int, val prefix: Option[String],
+class RiemannStatsLogger(val host: String, val port: Int,
+                         val prefix: Option[String],
                          _localHostname: Option[String],
                          val shortLocalHostname: Boolean,
+                         var tags: Seq[String] = Seq(),
                          val period: Duration, val collection: StatsCollection)
   extends PeriodicBackgroundProcess("RiemannStatsLogger", period) {
 
+  val logger = Logger.get(getClass.getName)
   val client = RiemannClient.tcp(host, port)
   val listener = new StatsListener(collection)
   val localHostname: String = _localHostname.getOrElse(getLocalHostname)
 
   def periodic() {
-    new JsonStatsLoggerConfig
-    client.connect()
-    val stats = listener.get()
-    val statMap =
-      stats.counters.map { case (key, value) => (key, value.doubleValue) } ++
-        stats.gauges ++
-        stats.metrics.flatMap { case (key, distribution) =>
-          distribution.toMap.map { case (subkey, value) =>
-            (key + "." + subkey, value.doubleValue)
+    try {
+      client.connect()
+      val stats = listener.get()
+      val statMap =
+        stats.counters.map { case (key, value) => (key, value.doubleValue) } ++
+          stats.gauges ++
+          stats.metrics.flatMap { case (key, distribution) =>
+            distribution.toMap.map { case (subkey, value) =>
+              (key + " " + subkey, value.doubleValue)
+            }
           }
-        }
 
+      val cleanedKeysStatMap = statMap.map { case (key, value) => (keyWithPrefix(key), value) }
 
-    val cleanedKeysStatMap = statMap.map { case (key, value) =>
-      (keyWithPrefix(key).replaceAll(":", "_").replaceAll("/", ".").replaceAll(" ", "_").toLowerCase(), value) }
+      val events = cleanedKeysStatMap.map { case (key, value) =>
+        newEvent
+          .service(key)
+          .metric(value)
+          .build()
+      }
 
-    val events = cleanedKeysStatMap.map { case (key, value) =>
-      client.event()
-        .service(key)
-        .host(localHostname)
-        .metric(value)
-        .ttl(period.inSeconds * 5)
-        .build()
+      client.sendEventsWithAck(events.toList)
+    } catch {
+      case ex: Throwable => logger.warning("Could not submit riemann metrics", ex)
     }
+  }
 
-    client.sendEventsWithAck(events.toList)
+  def newEvent = {
+    val event = client.event()
+
+    event.host(localHostname)
+    event.ttl(period.inSeconds * 5)
+    event.tags(tags)
+
+    event
   }
 
   def keyWithPrefix(key: String): String = {
-    prefix.map { p => "%s.%s".format(p, key) }.getOrElse(key)
+    prefix.map { p => "%s%s".format(p, key) }.getOrElse(key)
   }
 
   def getLocalHostname: String = {
-    val localHostname = InetAddress.getLocalHost.getHostName
+    val localHostname: String = InetAddress.getLocalHost.getHostName
 
     if (shortLocalHostname) {
       val regex = """^([^\.]+)""".r
